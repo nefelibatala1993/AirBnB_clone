fb9049943e978dca66c5a91c5ad223f34cb58c8b
nefelibatala1993 <leuela1993@gmail.com>
2022-10-29 14:28:31 +0300

    Added console.py

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines the HBNBCommand class"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command line interpreter for the AirBnB clone"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Quit program with EOF (Ctr-D)"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
