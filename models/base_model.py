d19429e96d068f74103a5d0f92bc4d94dc2fe37f
nefelibatala1993 <leuela1993@gmail.com>
2022-11-21 12:10:58 +0300

    Added base_model.py and test_base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/pyhton3
+"""Defines the ``BaseModel`` class"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methoods for other classes
+    Attributes:
+          id (str): unique ID of a new instance
+          created_at (datetime): current datetime when an instance is created
+          updated_at (datetime): current datetime when an instance is created, and
+            it will be updated everytime an object changes
+    """
+    def __init__(self):
+        """Instantiation Method/ Default Constructor"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of an instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__
+                                     , self.id, self.__dict__)
+
+    def save(self):
+        """Updates the ``updated_at`` datetime object"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of an object"""
+        dict_s = {}
+        for k, v in self.__dict__.items():
+            if isinstance(v, datetime):
+                dict_s[k] = v.isoformat()
+            else:
+                dict_s[k] = v
+        return dict_s
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines ``TestBaseModel`` class
+
+``TestBaseModel`` has unittests for the BaseModel class
+"""
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """Defines unittests for the BaseModel class"""
+    def setUp(self) -> None:
+        """SetUp method for initializing my new Instance Object"""
+        self.test_obj = BaseModel()
+
+    def test_id_is_str(self) -> None:
+        """Tests whether the new ID of an object is a string not a UUID object"""
+        self.assertIsInstance(self.test_obj.id, str)
+
+    def test_datetime_created_and_updated_at(self) -> None:
+        """Test whether the new objects created_at and updated_at attributes are
+        datetime objects."""
+        self.assertIsInstance(self.test_obj.created_at, datetime)
+        self.assertIsInstance(self.test_obj.updated_at, datetime)
+
+    def test_string_representation(self) -> None:
+        """Test the string representation of an object by calling the __str__ method
+        and also implicitly calling the method using print()"""
+        self.assertEqual(self.test_obj.__str__(),
+                         "[{}] ({}) {}".format(self.test_obj.__class__.__name__
+                                                , self.test_obj.id, self.test_obj.__dict__))
+
+    def test_to_dict_saves_dates_in_iso_format(self) -> None:
+        """Tests the to_dict method of an instance object"""
+        dict_s = self.test_obj.to_dict()
+        for k, v in self.test_obj.__dict__.items():
+            if isinstance(self.test_obj.__dict__[k], datetime):
+                self.assertEqual(datetime.fromisoformat(dict_s[k]), v)
+
