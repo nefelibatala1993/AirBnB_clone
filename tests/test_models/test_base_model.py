761bde4cdb004f5498a9d536476a8761b00765da
nefelibatala1993 <leuela1993@gmail.com>
2022-11-21 12:41:04 +0300

    Passed pycodestyle for test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,21 +15,24 @@ class TestBaseModel(unittest.TestCase):
         self.test_obj = BaseModel()
 
     def test_id_is_str(self) -> None:
-        """Tests whether the new ID of an object is a string not a UUID object"""
+        """Tests whether the new ID of an object is a string
+        not a UUID object"""
         self.assertIsInstance(self.test_obj.id, str)
 
     def test_datetime_created_and_updated_at(self) -> None:
-        """Test whether the new objects created_at and updated_at attributes are
-        datetime objects."""
+        """Test whether the new objects created_at and updated_at
+        attributes are datetime objects."""
         self.assertIsInstance(self.test_obj.created_at, datetime)
         self.assertIsInstance(self.test_obj.updated_at, datetime)
 
     def test_string_representation(self) -> None:
-        """Test the string representation of an object by calling the __str__ method
+        """Test the string representation of an object by calling
+        the __str__ method
         and also implicitly calling the method using print()"""
-        self.assertEqual(self.test_obj.__str__(),
-                         "[{}] ({}) {}".format(self.test_obj.__class__.__name__
-                                                , self.test_obj.id, self.test_obj.__dict__))
+        str_repr = "[{}] ({}) {}".format(self.test_obj.__class__.__name__,
+                                         self.test_obj.id,
+                                         self.test_obj.__dict__)
+        self.assertEqual(self.test_obj.__str__(), str_repr)
 
     def test_to_dict_saves_dates_in_iso_format(self) -> None:
         """Tests the to_dict method of an instance object"""
@@ -37,4 +40,3 @@ class TestBaseModel(unittest.TestCase):
         for k, v in self.test_obj.__dict__.items():
             if isinstance(self.test_obj.__dict__[k], datetime):
                 self.assertEqual(datetime.fromisoformat(dict_s[k]), v)
-
