1f82f823d1eb6e6827971982bc8103ab16ea2f83
nefelibatala1993 <leuela1993@gmail.com>
2022-11-25 09:40:45 +0300

    Added test_file_storage.p

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ Unittest classes:
     TestFileStorage_instantiation
     TestFileStorage_methods
 """
+import json
 import os
 import unittest
 from models.base_model import BaseModel
@@ -31,12 +32,19 @@ class TestFileStorage(unittest.TestCase):
 
     def test_new(self) -> None:
         """Tests the new method of when an object is created"""
-        test_base = BaseModel()
-        key = type(test_base).__name__ + "." + test_base.id
+        key = type(self.test_obj).__name__ + "." + self.test_obj.id
         FileStorage._FileStorage__objects = {}
-        storage.new(test_base)
+        storage.new(self.test_obj)
         dict_s = storage.all()
         self.assertIn(key, dict_s.keys())
+
+    def test_save(self) -> None:
+        """Tests the save method when the object are stored in JSON file"""
+        key = type(self.test_obj).__name__ + "." + self.test_obj.id
+        storage.save()
+        with open(FileStorage._FileStorage__file_path, "r", encoding="utf-8") as f:
+            dict_t = json.load(f)
+        self.assertIn(key, dict_t.keys())
 
 
 if __name__ == '__main__':
