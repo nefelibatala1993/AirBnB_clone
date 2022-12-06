1ceefec1240b2ead702db9f8f5cd55d77537751e
nefelibatala1993 <leuela1993@gmail.com>
2022-12-07 00:01:53 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,20 @@ from datetime import datetime
 
 class BaseModel:
     """Defines all the common attributes/methods for the other classes"""
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         """Instantiaion Method / Default Constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class___':
+                    if key != 'created_at' or key != 'updated_at':
+                        setattr(self, key, value)
+                    else:
+                        time = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        setattr(self, key, time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> None:
         """Prints the string representation of an instance"""
