09cc930607a1602863dd44ff738ff19ad4518c7f
nefelibatala1993 <leuela1993@gmail.com>
2022-12-20 18:40:40 +0300

    Update test_base_model.p

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,4 +6,9 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     """Defines unittests for the BaseModel class"""
-    
+    def setUp(self) -> None:
+        self.testBase = BaseModel()
+
+    def test_str(self) -> None:
+        """Tests the string representation of an object"""
+        test_str = f"[{self.testBase.__class__.__name__}] ({self.id}) {self.__dict__}"
