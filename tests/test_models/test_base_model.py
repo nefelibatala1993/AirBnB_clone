132034180e794831f28f1e14f624dae6228edf8e
nefelibatala1993 <leuela1993@gmail.com>
2022-12-20 18:44:56 +0300

    Test for save method of BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,6 +9,12 @@ class TestBaseModel(unittest.TestCase):
     def setUp(self) -> None:
         self.testBase = BaseModel()
 
+    def test_save(self) -> None:
+        """Tests save method of BaseModel when an object is saved"""
+        t = self.testBase.updated_at
+        self.testBase.save()
+        self.assertLess(t, self.testBase.updated_at)
+
     def test_str(self) -> None:
         """Tests the string representation of an object"""
         test_str = f"[{self.testBase.__class__.__name__}] ({self.id}) {self.__dict__}"
