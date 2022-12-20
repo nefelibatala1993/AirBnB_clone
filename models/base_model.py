4987fc11b6a3f57d4785b8f447822f206bc00eb0
nefelibatala1993 <leuela1993@gmail.com>
2022-12-20 18:35:19 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ class BaseModel:
     """Defines all common attributes/methods for other classes"""
     def __init__(self) -> None:
         """Instantiation Method(Default Constructor)"""
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
