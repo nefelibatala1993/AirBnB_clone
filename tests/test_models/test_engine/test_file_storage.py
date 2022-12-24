fb2d11642b680ef01bf96cc88dd4b9bcca09d329
nefelibatala1993 <leuela1993@gmail.com>
2022-12-25 02:28:37 +0300

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the TestFileStorage class"""
 import unittest
+import os
 from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
@@ -10,10 +11,16 @@ class TestFileStorage(unittest.TestCase):
     """Unittest for the FileStorage class"""
     def setUp(self) -> None:
         """Sets up for all the tests"""
-        pass
+        FileStorage._FileStorage__objects = {}
+
+    def tearDown(self) -> None:
+        """Cleans up after all tests have been carried out"""
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
     def test_all(self) -> None:
         """Tests the all method of the FileStorage class"""
+        testBase = BaseModel()
         self.assertDictEqual(FileStorage._FileStorage__objects, storage.all())
 
 
