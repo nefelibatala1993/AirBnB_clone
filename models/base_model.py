c15c4943d8fa0d9c8d9becbf56aebd2370f23de5
nefelibatala1993 <leuela1993@gmail.com>
2022-12-25 02:16:17 +0300

    Add FileStorage engine

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,8 @@
 import uuid
 from datetime import datetime
 
+import models
+
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
@@ -12,12 +14,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for key, val in kwargs.items():
                 if key == '__class__':
                     continue
                 elif key in ['created_at', 'updated_at']:
-                    setattr(self, key, datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key, datetime.strptime(val,
+                            '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, val)
 
@@ -28,6 +32,7 @@ class BaseModel:
     def save(self) -> None:
         """Updates an object"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self) -> dict:
         """Returns the dictionary representation of an object"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """`FileStorage` serializes instances to a JSON file and
+    deserializes JSON file to instances. This is one of the
+    storage engines that will power the AirBnB website"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self) -> dict:
+        """Returns the dictionary `__objects` that contains all
+        the objects created"""
+        return self.__objects
+
+    def new(self, obj) -> None:
+        """Sets in `__objects` the new object (obj) with the key
+        `<obj class name>.<obj id>`"""
+        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+
+    def save(self) -> None:
+        """Serializes __objects to JSON file (__file_path). This means
+        that the objects now will be stored in a file, to be recovered
+        later"""
+        json_dict = {key: val.to_dict() for key, val in self.__objects.items()}
+
+        # Save the objects to a JSON file
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_dict, f)
+
+    def reload(self) -> None:
+        """Deserializes JSON file (__file_path) to __objects. This wil reload
+        the objects from storage, hence retrieving objects from storage"""
+
+        try:
+            # Get the objects from the file
+            with open(self.__file_path, 'r') as f:
+                json_data = json.load(f)
+
+            # Set in __objects the objects retrieved appropriately
+            self.__objects = {
+                key: eval(val['__class__'])(**val)
+                for key, val in json_data.items()
+            }
+        except FileNotFoundError:
+            pass
